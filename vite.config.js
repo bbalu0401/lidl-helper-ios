0feb5d2cd7a31cfb4709a2aef317f94c35329fec
bbalu0401 <kt7njbhhws@privaterelay.appleid.com>
2025-11-02 20:51:50 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,10 @@
-export default { server: { port: 5173 } };
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  root: './', // index.html a gyökérben
+  build: {
+    outDir: 'dist'
+  }
+})
